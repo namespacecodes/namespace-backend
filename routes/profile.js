ce8c4acaf8746b93c0ed4e3b1380e8b2a78d713b
venkatarohan <venkatarohank@gmail.com>
2023-12-16 18:32:46 +0530

    added user_route

---
+++ routes/profile.js
@@ -0,0 +1,19 @@
+const router = require('./index')
+const {connection,db,runQuery}=require('../sqlconn')
+
+
+router.get('/:userRoute',async(req,res)=>{
+    const user_route=req.params.userRoute
+    try {
+        const data=await runQuery("getUserByUserRoute",{user_route})
+        return res.send({"status":"Success",data})
+    } catch (error) {
+        return res.status(400).send({"status":"Failed to get Profile"})
+    }
+})
+
+
+
+
+  
+module.exports = router;
