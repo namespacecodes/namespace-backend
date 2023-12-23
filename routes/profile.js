5e68c6d94592bc3aa7233424f24d44fe0364d17c
venkatarohan <venkatarohank@gmail.com>
2023-12-23 20:24:30 +0530

    updated user_route

--- routes/profile.js
+++ routes/profile.js
@@ -5,11 +5,14 @@ const {connection,db,runQuery}=require('../sqlconn')
 router.get('/:userRoute',async(req,res)=>{
     const user_route=req.params.userRoute
     try {
-        const data=await runQuery("getUserByUserRoute",{user_route})
-        return res.send({"status":"Success",data})
+        const user_data=await runQuery("getUserByUserRoute",{user_route})
+        const service_data=await runQuery("getServicesByUserId",{user_id:user_data.user_id})
+        const socialmedia_data=await runQuery("getSocialMediaProfileByUserId",{user_id:user_data.user_id})
+
+        return res.send({"status":"Success",user_data,service_data,socialmedia_data})
     } catch (error) {
         return res.status(400).send({"status":"Failed to get Profile"})
-        
+
     }
 })
 
